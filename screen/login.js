97fa741b8e8e072b993510f50b493082a3a39562
OrianaPalmero <lisettepalmero@gmail.com>
2024-10-14 21:47:05 -0300

    añadiendo splash

---
+++ screen/login.js
@@ -0,0 +1,14 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { useState,useEffect } from 'react'
+
+export default function login() {
+ 
+    return (
+      <View>
+        <Text>login</Text>
+      </View>
+    )
+  }
+
+
+const styles = StyleSheet.create({})
